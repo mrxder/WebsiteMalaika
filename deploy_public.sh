d50a4eb68dd4f9fbb87344d044184d3aa79cedbd
Matthias Keim <info@matthias-keim.com>
2019-03-31 16:12:36 +0200

    final v1

---
+++ deploy_public.sh
@@ -0,0 +1,4 @@
+jekyll build
+aws s3 sync _site s3://www.malaika.cc --exclude prev --profile keim
+sleep 10
+aws cloudfront create-invalidation --distribution-id E2FUHQH2ML2REJ --paths /* --profile keim
