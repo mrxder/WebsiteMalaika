ac12ad495352485166ef984ae4722900f34e16dd
Matthias Keim <info@matthias-keim.com>
2019-06-02 23:10:16 +0200

    changed deploy script

--- deploy_public.sh
+++ deploy_public.sh
@@ -1,4 +1,4 @@
 jekyll build
 aws s3 sync _site s3://www.malaika.cc --exclude prev --exclude SPE --profile keim
 sleep 10
-aws cloudfront create-invalidation --distribution-id E2FUHQH2ML2REJ --paths / --profile keim
+aws cloudfront create-invalidation --distribution-id E2FUHQH2ML2REJ --paths /* --profile keim
